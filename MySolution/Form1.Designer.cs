16693326305c99eb0477662342d8faef6f4da50d
MrKaz <MrKaz@DESKTOP-FK7J07S>
2021-09-09 17:44:22 +0400

    Add project files.

---
+++ MySolution/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace MySolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOriginalMessage = new System.Windows.Forms.TextBox();
+            this.buttonVerify = new System.Windows.Forms.Button();
+            this.textSignature = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textMessageDigestSHA1 = new System.Windows.Forms.TextBox();
+            this.buttonSign = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxQ = new System.Windows.Forms.TextBox();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxG = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonLoadText = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonLoadDSAParams = new System.Windows.Forms.Button();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textOriginalMessage
+            // 
+            this.textOriginalMessage.Location = new System.Drawing.Point(22, 30);
+            this.textOriginalMessage.Multiline = true;
+            this.textOriginalMessage.Name = "textOriginalMessage";
+            this.textOriginalMessage.Size = new System.Drawing.Size(614, 28);
+            this.textOriginalMessage.TabIndex = 9;
+            // 
+            // buttonVerify
+            // 
+            this.buttonVerify.Location = new System.Drawing.Point(516, 480);
+            this.buttonVerify.Name = "buttonVerify";
+            this.buttonVerify.Size = new System.Drawing.Size(120, 46);
+            this.buttonVerify.TabIndex = 17;
+            this.buttonVerify.Text = "Verify";
+            this.buttonVerify.Click += new System.EventHandler(this.buttonVerify_Click);
+            // 
+            // textSignature
+            // 
+            this.textSignature.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSignature.Location = new System.Drawing.Point(22, 150);
+            this.textSignature.Multiline = true;
+            this.textSignature.Name = "textSignature";
+            this.textSignature.ReadOnly = true;
+            this.textSignature.Size = new System.Drawing.Size(614, 28);
+            this.textSignature.TabIndex = 14;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(12, 123);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(634, 64);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Digital Signature";
+            // 
+            // textMessageDigestSHA1
+            // 
+            this.textMessageDigestSHA1.Location = new System.Drawing.Point(22, 86);
+            this.textMessageDigestSHA1.Multiline = true;
+            this.textMessageDigestSHA1.Name = "textMessageDigestSHA1";
+            this.textMessageDigestSHA1.ReadOnly = true;
+            this.textMessageDigestSHA1.Size = new System.Drawing.Size(614, 28);
+            this.textMessageDigestSHA1.TabIndex = 12;
+            // 
+            // buttonSign
+            // 
+            this.buttonSign.Location = new System.Drawing.Point(12, 480);
+            this.buttonSign.Name = "buttonSign";
+            this.buttonSign.Size = new System.Drawing.Size(120, 46);
+            this.buttonSign.TabIndex = 11;
+            this.buttonSign.Text = "Sign";
+            this.buttonSign.Click += new System.EventHandler(this.buttonSign_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Controls.Add(this.textBoxQ);
+            this.groupBox5.Controls.Add(this.textBoxP);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Controls.Add(this.textBoxY);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.textBoxG);
+            this.groupBox5.Location = new System.Drawing.Point(12, 197);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(634, 277);
+            this.groupBox5.TabIndex = 16;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "DSA Parameters";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Q";
+            // 
+            // textBoxQ
+            // 
+            this.textBoxQ.Location = new System.Drawing.Point(29, 83);
+            this.textBoxQ.Multiline = true;
+            this.textBoxQ.Name = "textBoxQ";
+            this.textBoxQ.ReadOnly = true;
+            this.textBoxQ.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxQ.Size = new System.Drawing.Size(595, 55);
+            this.textBoxQ.TabIndex = 5;
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Location = new System.Drawing.Point(29, 18);
+            this.textBoxP.Multiline = true;
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.ReadOnly = true;
+            this.textBoxP.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxP.Size = new System.Drawing.Size(595, 56);
+            this.textBoxP.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(10, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(29, 148);
+            this.textBoxY.Multiline = true;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.ReadOnly = true;
+            this.textBoxY.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxY.Size = new System.Drawing.Size(595, 55);
+            this.textBoxY.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(10, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "G";
+            // 
+            // textBoxG
+            // 
+            this.textBoxG.Location = new System.Drawing.Point(29, 212);
+            this.textBoxG.Multiline = true;
+            this.textBoxG.Name = "textBoxG";
+            this.textBoxG.ReadOnly = true;
+            this.textBoxG.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxG.Size = new System.Drawing.Size(595, 56);
+            this.textBoxG.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(634, 55);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Message";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(12, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(634, 56);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "SHA1 Hash Bytes";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(138, 480);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(120, 46);
+            this.buttonSave.TabIndex = 18;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoadText
+            // 
+            this.buttonLoadText.Location = new System.Drawing.Point(264, 480);
+            this.buttonLoadText.Name = "buttonLoadText";
+            this.buttonLoadText.Size = new System.Drawing.Size(120, 46);
+            this.buttonLoadText.TabIndex = 19;
+            this.buttonLoadText.Text = "Load";
+            this.buttonLoadText.UseVisualStyleBackColor = true;
+            this.buttonLoadText.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonLoadDSAParams
+            // 
+            this.buttonLoadDSAParams.Location = new System.Drawing.Point(390, 480);
+            this.buttonLoadDSAParams.Name = "buttonLoadDSAParams";
+            this.buttonLoadDSAParams.Size = new System.Drawing.Size(120, 46);
+            this.buttonLoadDSAParams.TabIndex = 20;
+            this.buttonLoadDSAParams.Text = "Load DSA";
+            this.buttonLoadDSAParams.UseVisualStyleBackColor = true;
+            this.buttonLoadDSAParams.Click += new System.EventHandler(this.buttonLoadDSAParams_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 534);
+            this.Controls.Add(this.buttonLoadDSAParams);
+            this.Controls.Add(this.buttonLoadText);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textOriginalMessage);
+            this.Controls.Add(this.buttonVerify);
+            this.Controls.Add(this.textSignature);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.textMessageDigestSHA1);
+            this.Controls.Add(this.buttonSign);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textOriginalMessage;
+        private System.Windows.Forms.Button buttonVerify;
+        private System.Windows.Forms.TextBox textSignature;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textMessageDigestSHA1;
+        private System.Windows.Forms.Button buttonSign;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxQ;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxG;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonLoadText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonLoadDSAParams;
+    }
+}
+
---
+++ MySolution/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DSACryptoServiceProvider dsa;
+        DSAParameters dsaparams;
+        byte[] signaturebytes;
+
+        private void buttonSign_Click(object sender, EventArgs e)
+        {
+            byte[] messagebytes = Encoding.UTF8.GetBytes(textOriginalMessage.Text);
+            
+            
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] hashbytes =sha.ComputeHash(messagebytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashbytes.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", hashbytes[i]));
+            }
+            textMessageDigestSHA1.Text = sb.ToString();
+
+
+            dsa = new DSACryptoServiceProvider();
+            signaturebytes = dsa.SignHash(hashbytes, "1.3.14.3.2.26");
+
+            sb = new StringBuilder();
+            for (int i = 0; i < signaturebytes.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", signaturebytes[i]));
+            }
+            textSignature.Text = sb.ToString();
+
+
+            dsaparams = dsa.ExportParameters(false);
+            sb = new StringBuilder();
+            for (int i = 0; i < dsaparams.P.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", dsaparams.P[i]));
+            }
+            textBoxP.Text = sb.ToString();
+            sb = new StringBuilder();
+            for (int i = 0; i < dsaparams.Q.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", dsaparams.Q[i]));
+            }
+            textBoxQ.Text = sb.ToString();
+            for (int i = 0; i < dsaparams.Y.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", dsaparams.Y[i]));
+            }
+            textBoxY.Text = sb.ToString();
+            for (int i = 0; i < dsaparams.G.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", dsaparams.G[i]));
+            }
+            textBoxG.Text = sb.ToString();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр4 DigitalSignature";
+
+            MessageBox.Show("Choose Place For Text.");
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string textName = saveFileDialog.FileName;
+                System.IO.File.WriteAllText(textName, textOriginalMessage.Text);
+
+
+                MessageBox.Show("Choose Place For DSA Params.");
+                saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string dsaParamsName = saveFileDialog.FileName;
+                    System.IO.File.WriteAllText(dsaParamsName, dsa.ToXmlString(false));
+
+
+                    MessageBox.Show("Choose Place For Signature.");
+                    saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string signatureName = saveFileDialog.FileName;
+                        System.IO.File.WriteAllBytes(signatureName, signaturebytes);
+                        MessageBox.Show("Success.");
+                    }
+                }
+            }
+
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр4 DigitalSignature";
+
+            MessageBox.Show("Load Text.");
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string textFileName = openFileDialog.FileName;
+                    textOriginalMessage.Text = System.IO.File.ReadAllText(textFileName);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void buttonLoadDSAParams_Click(object sender, EventArgs e)
+        {
+            dsa = new DSACryptoServiceProvider();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр4 DigitalSignature";
+
+            MessageBox.Show("Load DSA Params.");
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string dsaParamsFileName = openFileDialog.FileName;
+                    string xmlParams = System.IO.File.ReadAllText(dsaParamsFileName);
+                    dsa.FromXmlString(xmlParams);
+
+                    dsaparams = dsa.ExportParameters(false);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < dsaparams.P.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", dsaparams.P[i]));
+                    }
+                    textBoxP.Text = sb.ToString();
+                    sb = new StringBuilder();
+                    for (int i = 0; i < dsaparams.Q.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", dsaparams.Q[i]));
+                    }
+                    textBoxQ.Text = sb.ToString();
+                    for (int i = 0; i < dsaparams.Y.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", dsaparams.Y[i]));
+                    }
+                    textBoxY.Text = sb.ToString();
+                    for (int i = 0; i < dsaparams.G.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", dsaparams.G[i]));
+                    }
+                    textBoxG.Text = sb.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void buttonVerify_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр4 DigitalSignature";
+
+            MessageBox.Show("Load Signature.");
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string signatureFileName = openFileDialog.FileName;
+                    signaturebytes =System.IO.File.ReadAllBytes(signatureFileName);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < signaturebytes.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", signaturebytes[i]));
+                    }
+                    textSignature.Text = sb.ToString();
+
+                    byte[] messagebytes = Encoding.UTF8.GetBytes(textOriginalMessage.Text);
+                    SHA1 sha = new SHA1CryptoServiceProvider();
+                    byte[] hashbytes = sha.ComputeHash(messagebytes);
+                    sb = new StringBuilder();
+                    for (int i = 0; i < hashbytes.Length; i++)
+                    {
+                        sb.Append(String.Format("{0,2:X2} ", hashbytes[i]));
+                    }
+                    textMessageDigestSHA1.Text = sb.ToString();
+
+
+
+                    bool match = dsa.VerifyHash(hashbytes, "1.3.14.3.2.26", signaturebytes);
+
+                    String strResult;
+                    if (match)
+                        strResult = "VerifySignature returned TRUE";
+                    else
+                        strResult = "VerifySignature returned FALSE";
+                    MessageBox.Show(strResult, "Result From Calling VerifySignature", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
